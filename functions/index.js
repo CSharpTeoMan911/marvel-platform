d68c5ac7d3ed3d960fedf94d7c19a9e1de16e7c5
Teodor Mihail <teodormihail07@gmail.com>
2024-12-19 21:11:02 +0000

    * IMPLEMENTED AND FIXED SIGN UP FUNCTION (*TESTING ENVIRONMENT ONLY)

--- functions/index.js
+++ functions/index.js
@@ -18,12 +18,35 @@ const { seedDatabase } = require('./cloud_db_seed');
 
 seedDatabase();
 
-app.get('/', (req, res) => res.json({"Test":"Test"}));
+
+// Server functions served as a CRUD API interface
+//
+// [BEGIN]
+
+app.get('/signUpUser', async (req, res) => {
+    res.json(await userController.signUpUser(req.query));
+}
+);
+
+
+// app.get('/signUpUser', async (req, res) => {
+//     res.json(await userController.signUpUser(req.query));
+// }
+// );
+
+
+// [END]
+
 
 /* Migration Scripts */
 // const {
 // } = require("./migrationScripts/modifyChallengePlayersData");
 const migrationScripts = {};
 
-
+// Serve all the functions as a single Google Cloud Function in order to 
+// enable 'CORS'(Cross-origin resource sharing) by passing an 'Express' 
+// CORS enabled API app object, to the 'onRequest' firebase function. 
+// This must be done because the front-end web-app and the server app
+// are served at 2 different URLs (origins), and the server must be 
+// configured to accept requests from different origins.
 module.exports.functions = https.onRequest(app);
--- libs/services/user/signUp.js
+++ libs/services/user/signUp.js
@@ -1,29 +1,47 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { httpsCallable } from 'firebase/functions';
-
-import { sendVerification } from './manageUser';
 
 import { AUTH_ERROR_MESSAGES } from '@/libs/constants/auth';
 
 import { auth, functions } from '@/libs/redux/store';
+
+import { sendVerification } from './manageUser';
+
+
 import axios from "axios"
+
+import { EMULATOR_FUNCTIONS, PRODUCTION_FUNCTIONS } from "../../constants/functions"
 
 const signUp = async (email, password, fullName) => {
   try {
-    const r = await axios.get("http://127.0.0.1:5001/marvel-platform-c3a0b/us-central1/functions");
-    console.log(r);
-    // const createUser = httpsCallable(functions, 'signUpUser');
 
-    // const response = await createUserWithEmailAndPassword(
-    //   auth,
-    //   email,
-    //   password
-    // );
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then(async (auth_response) => {
+        try {
 
-    // await createUser({ email, fullName, uid: response.user.uid });
-    // await sendVerification(response.user);
+          await sendVerification(auth_response.user);
 
-    return response.user;
+          const response = await axios.get(`${EMULATOR_FUNCTIONS.signUpUser}`, {
+            params: {
+              email: email,
+              fullName: fullName,
+              uid: auth_response.user.uid
+            }
+          });
+
+          if (response.status == 200 && response.data.status == 'success') {
+            return auth_response.user;
+          }
+          else {
+            console.log(response.message);
+            throw new Error(response.message);
+          }
+
+        } catch (error) {
+          console.log(error);
+          throw new Error(AUTH_ERROR_MESSAGES[error?.code]);
+        }
+      });
+
   } catch (error) {
     throw new Error(AUTH_ERROR_MESSAGES[error?.code]);
   }
